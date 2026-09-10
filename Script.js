c5f4c9ac1073582562d414e98fef611927fb8efd
Jeerasakjalern <jeerasak.jalernpon@gmail.com>
2026-09-10 14:17:06 +0700

    updated from class

--- Script.js
+++ Script.js
@@ -1,3 +1,5 @@
 console.log("Hello Javascript");
 console.log("Hello Computer Science");
 document.getElementById("text").innerHTML = "This is first time for javaScript";
+document.write("Hello document write");
+window.alert("Welcome to my page");
